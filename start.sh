d74e3f239c25a2583d6458f259478754820a7d6d
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2023-01-18 12:47:12 -0300

    update start.sh

--- start.sh
+++ start.sh
@@ -11,11 +11,10 @@ java \
   -XX:+UseG1GC \
   -XX:+UnlockExperimentalVMOptions \
   -XX:InitiatingHeapOccupancyPercent=24 \
-  -XX:ConcGCThreads=16 \
   -XX:G1HeapRegionSize=128M \
   -XX:G1HeapWastePercent=1 \
   -XX:G1MixedGCLiveThresholdPercent=65 \
   -XX:G1RSetUpdatingPauseTimePercent=4 \
-  -XX:G1ConcRefinementThreads=16 \
+  -XX:+AlwaysPreTouch \
   -Xmx1G \
   -jar velocity.jar
