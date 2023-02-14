f90eff0724d8cd712435f55f7586b659776d20be
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2023-02-13 23:08:34 -0300

    Updated proxy to 2.0.0

--- start.sh
+++ start.sh
@@ -15,13 +15,6 @@ echo "Everything is up to date"
 
 echo "Starting proxy"
 java \
-  -XX:+UseG1GC \
-  -XX:+UnlockExperimentalVMOptions \
-  -XX:InitiatingHeapOccupancyPercent=24 \
-  -XX:G1HeapRegionSize=128M \
-  -XX:G1HeapWastePercent=1 \
-  -XX:G1MixedGCLiveThresholdPercent=65 \
-  -XX:G1RSetUpdatingPauseTimePercent=4 \
   -XX:+AlwaysPreTouch \
   -Xmx700M \
   -jar velocity.jar
